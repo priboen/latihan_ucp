6e87442afb89c12a9f37e05a11fe9840b8f8c96f
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-11-22 23:07:57 +0700

    .gitignore is now working

--- src/configs/database.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    multipleStatements  : true,
-    host                : 'localhost',
-    user                : 'root',
-    password            : '05Januari2003!',
-    database            : 'prak_paw_db'
-};
