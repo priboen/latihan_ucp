b073fb02621d30211ee2ba70f4f97ab6d433cd5b
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2024-12-08 14:18:53 +0700

    fix typo

--- src/routes/router-contact.js
+++ src/routes/router-contact.js
@@ -3,10 +3,10 @@ const contactController = require('../controllers').contact;
 const verifyUser = require('../configs/verify');
 
 router.get('/', verifyUser.isLogin, contactController.getContact);
-router.get('/contact/add', verifyUser.isLogin, contactController.formContact);
-router.post('/contact/save', verifyUser.isLogin, contactController.saveContact);
-router.get('/contact/edit/:id', verifyUser.isLogin, contactController.editContact);
-router.post('/contact/update/:id', verifyUser.isLogin, contactController.updateContact);
-router.get('/contact/delete/:id', verifyUser.isLogin, contactController.deleteContact);
+router.get('/add', verifyUser.isLogin, contactController.formContact);
+router.post('/save', verifyUser.isLogin, contactController.saveContact);
+router.get('/edit/:id', verifyUser.isLogin, contactController.editContact);
+router.post('/update/:id', verifyUser.isLogin, contactController.updateContact);
+router.get('/delete/:id', verifyUser.isLogin, contactController.deleteContact);
 
 module.exports = router;
